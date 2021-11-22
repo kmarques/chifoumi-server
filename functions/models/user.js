e56a22fbd4b6bbc27c66ae5c8625a1ad53ee172d
Karl MARQUES <kmarques@vetixy.com>
2021-11-22 12:01:12 +0100

    First commit

---
+++ functions/models/user.js
@@ -0,0 +1,13 @@
+const Mongoose = require("mongoose");
+
+const UserSchema = new Mongoose.Schema({
+  _id: {type: String, required: true},
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = Mongoose.model("User", UserSchema);
+
+module.exports = User;
