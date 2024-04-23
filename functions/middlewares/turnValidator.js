ed8d454fd78b7a963dcc463bddca22da78f0d643
MARQUES Karl <karlm2308@gmail.com>
2024-04-23 15:42:21 +0200

    add move validation

--- functions/middlewares/turnValidator.js
+++ functions/middlewares/turnValidator.js
@@ -14,6 +14,9 @@ module.exports = async (req, res, next) => {
   if (lastTurn + 1 !== idTurn - 1) {
     return res.status(400).json({turn: "not last"});
   }
+  if (!["scissors", "rock", "paper"].includes(req.body.move)) {
+    return res.status(400).json({turn: "Invalid move, expected: scissors, rock, paper"});
+  }
   const turn = match.turns[idTurn - 1] || {};
   const isPlayer1 = match.user1._id === req.user._id;
   const isPlayer2 = match.user2._id === req.user._id;
