845d54b2931a0fe1ac9d3485af9cc295d7f47657
MARQUES Karl <karl@kmarques.dev>
2025-01-06 01:46:15 +0100

    Update turnValidator.js
    fix when second player not exists yet

--- functions/middlewares/turnValidator.js
+++ functions/middlewares/turnValidator.js
@@ -18,8 +18,8 @@ module.exports = async (req, res, next) => {
     return res.status(400).json({turn: "Invalid move, expected: scissors, rock, paper"});
   }
   const turn = match.turns[idTurn - 1] || {};
-  const isPlayer1 = match.user1._id === req.user._id;
-  const isPlayer2 = match.user2._id === req.user._id;
+  const isPlayer1 = match.user1?._id === req.user._id;
+  const isPlayer2 = match.user2?._id === req.user._id;
   if ((isPlayer1 && turn.user1) || (isPlayer2 && turn.user2)) {
     return res.status(400).json({user: "move already given"});
   }
